ff5981781c9aec9a90003db17f7da05c67325df8
magasoov <77aushev@mail.ru>
2024-05-08 19:20:19 +0200

    magasov

--- assets/script/backend/app.js
+++ assets/script/backend/app.js
@@ -19,6 +19,26 @@ let products = [
         name: "Гипсокартон Волма, 2500 х 1200 х 12,5 мм",
         price: "335 ₽"
     },
+    {
+        image: "./assets/images/home/popular/popular1.png",
+        name: "Керамогранит Yasmin 598х185 коричневый C-YA4M112D",
+        price: "899 ₽"
+    },
+    {
+        image: "./assets/images/home/popular/popular2.png",
+        name: "Затирка для узких швов Ceresit СЕ 33, цвет белый, 2 кг",
+        price: "275 ₽"
+    },
+    {
+        image: "./assets/images/home/popular/popular3.png",
+        name: "Грунтовка Старатели 10л глубокого проникновения",
+        price: "839 ₽"
+    },
+    {
+        image: "./assets/images/home/popular/popular4.png",
+        name: "Гипсокартон Волма, 2500 х 1200 х 12,5 мм",
+        price: "335 ₽"
+    },
 
 ];
 
