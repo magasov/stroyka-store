3602337add3810f34b329e92705f83d8e78624fa
magasoov <77aushev@mail.ru>
2024-05-08 19:29:00 +0200

    magasov

--- assets/script/backend/app.js
+++ assets/script/backend/app.js
@@ -21,43 +21,43 @@ let products = [
     },
     {
         image: "./assets/images/home/popular/popular5.png",
-        name: "Керамогранит Yasmin 598х185 коричневый C-YA4M112D",
-        price: "899 ₽"
+        name: "Профиль 0,55мм для гипсокартона",
+        price: "195 ₽"
     },
     {
         image: "./assets/images/home/popular/popular6.png",
-        name: "Затирка для узких швов Ceresit СЕ 33, цвет белый, 2 кг",
-        price: "275 ₽"
+        name: "Рулетка 3м",
+        price: "100 ₽"
     },
     {
         image: "./assets/images/home/popular/popular7.png",
-        name: "Грунтовка Старатели 10л глубокого проникновения",
-        price: "839 ₽"
+        name: "Кнауф Мп 75 штукатурка гипсовая МН 30кг",
+        price: "380 ₽"
     },
     {
         image: "./assets/images/home/popular/popular8.png",
-        name: "Гипсокартон Волма, 2500 х 1200 х 12,5 мм",
+        name: "Уголок серый канализационный Д110 ГР90",
         price: "335 ₽"
     },
     {
         image: "./assets/images/home/popular/popular9.png",
-        name: "Керамогранит Yasmin 598х185 коричневый C-YA4M112D",
-        price: "899 ₽"
+        name: "Knauf Ротбанд, 30 кг",
+        price: "508 ₽"
     },
     {
         image: "./assets/images/home/popular/popular10.png",
-        name: "Затирка для узких швов Ceresit СЕ 33, цвет белый, 2 кг",
-        price: "275 ₽"
+        name: "Саморез по металлу 3,5х25 мм для гипсокартона",
+        price: "340 ₽"
     },
     {
         image: "./assets/images/home/popular/popular11.png",
-        name: "Грунтовка Старатели 10л глубокого проникновения",
-        price: "839 ₽"
+        name: "Шпатлевка универсальная Danogips SuperFinish 17 л",
+        price: "2555 ₽"
     },
     {
         image: "./assets/images/home/popular/popular12.png",
-        name: "Гипсокартон Волма, 2500 х 1200 х 12,5 мм",
-        price: "335 ₽"
+        name: "Клейкая лента металлизированная Изоспан FL 5х5000 см",
+        price: "226 ₽"
     },
 
 ];
