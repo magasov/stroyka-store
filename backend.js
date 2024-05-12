81cdb4eebe3ef298f9a2f91bd47359d6b88083f2
magasoov <77aushev@mail.ru>
2024-05-12 16:00:39 +0300

    magasov

--- backend.js
+++ backend.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 3000;
-
 const path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-
 app.use(bodyParser.json());
 
 let cartItems = [];
+let orders = []; // Создаем массив для хранения информации о заказах пользователя
 
 app.post('/addToCart', (req, res) => {
     const { name, price, image } = req.body;
@@ -20,6 +18,20 @@ app.post('/addToCart', (req, res) => {
 
 app.get('/getCartItems', (req, res) => {
     res.json(cartItems);
+});
+
+// Обработка запроса на получение информации о заказах
+app.get('/orders', (req, res) => {
+    // Отправляем информацию о заказах клиенту
+    res.json(orders);
+});
+
+// Обработка запроса на добавление заказа
+app.post('/completeOrder', (req, res) => {
+    const order = req.body;
+    orders.push(order); // Добавляем новый заказ в массив заказов
+    cartItems = []; // Очищаем корзину после завершения заказа
+    res.status(200).send('Заказ успешно оформлен');
 });
 
 app.listen(PORT, () => {
